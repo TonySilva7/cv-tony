1dda109174b62ffc4c254631a76a6605df64270a
TonyDev77 <tony.music7@gmail.com>
2021-01-21 11:59:35 -0300

    Finalização de Habilidades e começo da implantação de responsividade

--- src/Profile/Formacao.tsx
+++ src/Profile/Formacao.tsx
@@ -10,7 +10,6 @@ function Formacao() {
 		<div className="about-content" id="formacao">
 			<h1 className="container-title">FORMAÇÃO</h1>
 			<section className="container-description">
-
 				<span className="formation-container">
 					<img src={ImgBrushes} alt="Imagem do perfil" className="icon-formation"/>
 					<h3>ENSINO MÉDIO</h3>
@@ -26,7 +25,7 @@ function Formacao() {
 					<h3>MARKETING</h3>
 					<article>
 						<p><strong>Instituição</strong>: UNINTER </p>
-						<p><strong>Nível</strong>: Superior </p>
+						<p><strong>Nível</strong>: Ensino Superior </p>
 						<p><strong>Conclusão</strong>: 2018 </p>
 					</article>
 				</span>
--- src/Profile/Habilidades.tsx
+++ src/Profile/Habilidades.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import ImgFront from './icon-frontend.png';
+import ImgBack from './icon-backend.png';
+import ImgWeb from './icon-webdesign.png';
+import ImgVideo from './icon-makemove.png';
+import ImgAudio from './icon-audioedition.png';
+import ImgScrum from './icon-scrum.png';
+import ImgGit from './icon-git.png';
+import ImgLang from './icon-language.png';
 
 function Habilidades() {
 
@@ -6,20 +14,91 @@ function Habilidades() {
 		<>
 		<div className="about-content" id="habilidades">
 			<h1 className="container-title">HABILIDADES</h1>
-			<section className="container-description">
-				<div className="content-icon">
 
-				</div>
-				<article className="content-article">
-					<p className="description-text">
-						O comportamento do D. n. minor provavelmente não diferiu muito do comportamento do emu do
-						continente. As aves se reuniam em bandos para forragear e durante o período de reprodução.
-						Alimentavam-se de frutas, grama e algas marinhas. Corriam rapidamente e se defendiam com chutes.
-						O ninho era raso e feito com folhas mortas e musgo. Eram postos de sete a nove ovos, os quais
-						eram incubados por ambos os pais.
-					</p>
-				</article>
+			<section className="container-description cont-desc-skill">
+				<span className="skill-container">
+					<img src={ImgFront} alt="Imagem do perfil" className="icon-skill"/>
+					<h3>FRONT END</h3>
+					<article>
+						<p><strong>HTML & CSS</strong>: (FlexBox, Grid) </p>
+						<p><strong>JavaScript</strong></p>
+						<p><strong>Bootstrap</strong></p>
+						<p><strong>JQuery</strong></p>
+					</article>
+				</span>
+
+				<span className="skill-container">
+					<img src={ImgBack} alt="Imagem do perfil" className="icon-skill"/>
+					<h3>BACK END</h3>
+					<article>
+						<p><strong>UML</strong></p>
+						<p><strong>Java</strong> | Spring Boot </p>
+						<p><strong>Node</strong> | Angular </p>
+						<p><strong>PHP</strong> | Symfony </p>
+						<p><strong>MySQL</strong></p>
+						<p><strong>PostgreSQL</strong></p>
+					</article>
+				</span>
+
+				<span className="skill-container">
+					<img src={ImgWeb} alt="Imagem do perfil" className="icon-skill"/>
+					<h3>WEB DESIGN</h3>
+					<article>
+						<p><strong>Photoshop</strong>: Adobe </p>
+						<p><strong>Ilustrator</strong>: Adobe </p>
+						<p><strong>Figma</strong></p>
+					</article>
+				</span>
+
+				<span className="skill-container">
+					<img src={ImgGit} alt="Imagem do perfil" className="icon-skill"/>
+					<h3>GIT</h3>
+					<article>
+						<p><strong>Git Hub</strong></p>
+					</article>
+				</span>
+
+				<span className="skill-container">
+					<img src={ImgScrum} alt="Imagem do perfil" className="icon-skill"/>
+					<h3>AGILE METHOD.</h3>
+					<article>
+						<p><strong>Scrum</strong></p>
+					</article>
+				</span>
+
+				<span className="skill-container">
+					<img src={ImgLang} alt="Imagem do perfil" className="icon-skill"/>
+					<h3>IDIOMAS</h3>
+					<article>
+						<p><strong>Inglês</strong>: intermediário</p>
+						<p><strong>Espanhol</strong>: intermediário</p>
+					</article>
+				</span>
+
+				<span className="skill-container">
+					<img src={ImgVideo} alt="Imagem do perfil" className="icon-skill"/>
+					<h3>MAKE MOVIE</h3>
+					<article>
+						<p><strong>Premiere</strong>: Adobe </p>
+						<p><strong>Final Cut</strong>: Apple </p>
+						<p><strong>After Effects</strong>: Adobe </p>
+						<p><strong>Motion</strong>: Apple </p>
+						<p><strong>Cinema 4d</strong></p>
+					</article>
+				</span>
+
+				<span className="skill-container">
+					<img src={ImgAudio} alt="Imagem do perfil" className="icon-skill"/>
+					<h3>AUDIO EDITION</h3>
+					<article>
+						<p><strong>Pro Tools</strong></p>
+						<p><strong>Logic</strong>: Apple </p>
+						<p><strong>Audition</strong>: Adobe</p>
+					</article>
+				</span>
+
 			</section>
+
 		</div>
 		</>
 	)
