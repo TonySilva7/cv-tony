8705b7b153879db1c2c8cc31d89b149ec47fec8e
TonySilva7 <tony.deve7oper@gmail.com>
2023-08-18 23:33:27 -0300

    refactor: update info about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -27,20 +27,19 @@ function About() {
 					<div></div>
 					<article>
 						<p>
-							Sou graduado em Análise e Desenvolvimento de Sistemas,
-							Marketing e atualmente concluindo MBA em Engenharia de Software pela UTFPR - Paraná.
-							Atuo com desenvolvimento de sistemas web, mobile e API's.
+							Olá! Me chamo Tony, sou formado em Análise e Desenvolvimento de Sistemas e com um MBA em Engenharia de Software pela renomada Universidade Tecnológica Federal do Paraná.
 						</p>
 						<p>
-							Sou apaixonado por conhecimento, tecnologia e inovação, por isso
-							há muito tempo decidi mudar completamente de carreira, (sim, eu era professor de música em uma faculdade &#128512;) e explorar
-							essa área que tem crescido exponencialmente nos últimos anos — tecnologia.
+							Com experiência sólida em tecnologias front-end como ReactJS, NextJS, TypeScript, JS, Redux, entre outros, crio experiências digitais excepcionais, garantindo a compatibilidade e acessibilidade em diferentes dispositivos.
 						</p>
 						<p>
-							Aprendi a cultivar bons relacionamentos e respeitar as pessoas,
-							valorizando-as pelo ser humano que são. Trabalho muito bem em equipe e liderança, também sou capaz de me
-							adaptar à rápidas mudanças e gosto de um bom desafio. Em menos de um minutinho de leitura você encontrará mais
-							detalhes abaixo. Aguardo seu contato e até breve!
+							Minha abordagem se baseia em metodologias ágeis, facilitando a colaboração em equipe e liderança quando necessário. Tenho habilidades em integração e entrega contínua, garantindo a qualidade do software em todos os estágios.
+						</p>
+						<p>
+							Minhas habilidades vão além do front-end, pois atuo também no back-end e desenvolvimento mobile. Acredito também na importância das relações interpessoais no ambiente profissional, valorizando a colaboração e o respeito mútuo. Mantenho-me atualizado com as últimas tendências para trazer inovação aos projetos.
+						</p>
+						<p>
+						Seja para um projeto desafiador ou para discutir ideias inovadoras, estou ansioso para trocar experiências e contribuir. Entre em contato e vamos construir algo incrível juntos. Até breve!"
 						</p>
 					</article>
 				</main>
